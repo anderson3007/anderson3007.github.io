6afa41abd14534b0297735e5918a66231042231c
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-16 14:27:38 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,11 +1,11 @@
 let img;
 function preload() {
-  im = loadImage('img/run_direito/Ru.png');
+  //im = loadImage('img/run_direito/Ru.png');
 }
 function setup() {
   createCanvas(1200, 552);
   background(50);
   // Top-left corner of the img is at (10, 10)
   // Width and height are 50 x 50
-  image(img, 500, 100, 50, 50);
+ // image(img, 500, 100, 50, 50);
 }
