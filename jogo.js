fa9d6b4010ff8e4ff4e5ddee6ebd4a4af666081a
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-25 19:57:45 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -6,14 +6,8 @@ let pararD=[ ]
 let pararE=[ ]
 
 function preload {
-        img = loadImage('img/livro.png');
     for (var i=0;i<=9;i++){
         andarD[i]= loadImage('img/run_direito/Run__00'+i+'.png');
-        andarE[i]= loadImage('img/run_esquerdo/Runv__00'+i+'png');
-        morrerD[i]= loadImage('img/dead_direito/Dead__00'+i+'png');
-        morrerE[i]= loadImage('img/dead_esquerdo/Deadv__00'+i+'png');
-        pararD[i]= loadImage('img/idle_direito/Idle__00'+i+'png');
-        pararE[i]= loadImage('img/idle_esquerdo/Idlev__00'+i+'png');
     }
 
 }
@@ -28,7 +22,7 @@ function draw() {
   background(200);
   fill(250,250,250);
   for(var j=0; j<=9; j++) {
-      image(andarD[j], x, y, 30,30);
+      image(andarD[j],100, 100, 30,30);
     }
   
 }
