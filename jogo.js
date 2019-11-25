ccc77be5de774295dccdbceb14c1a1906d5299cc
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-25 19:55:28 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,6 +1,22 @@
+let andarD= [ ]
+let andarE=[ ]
+let morrerD=[ ]
+let morrerE=[ ]
+let pararD=[ ]
+let pararE=[ ]
 
+function preload {
+        img = loadImage('img/livro.png');
+    for (var i=0;i<=9;i++){
+        andarD[i]= loadImage('img/run_direito/Run__00'+i+'.png');
+        andarE[i]= loadImage('img/run_esquerdo/Runv__00'+i+'png');
+        morrerD[i]= loadImage('img/dead_direito/Dead__00'+i+'png');
+        morrerE[i]= loadImage('img/dead_esquerdo/Deadv__00'+i+'png');
+        pararD[i]= loadImage('img/idle_direito/Idle__00'+i+'png');
+        pararE[i]= loadImage('img/idle_esquerdo/Idlev__00'+i+'png');
+    }
 
-
+}
 
 function setup() {
   createCanvas(1200, 540);
@@ -8,8 +24,13 @@ function setup() {
 
 
 function draw() {
+  
   background(200);
   fill(250,250,250);
+  for(var j=0; j<=9; j++) {
+      image(andarD[j], x, y, 30,30);
+    }
+  
 }
 
   
