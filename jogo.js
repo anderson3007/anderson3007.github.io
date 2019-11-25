456e9e205e124a1902d133922f5876e39bace561
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-25 20:04:26 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,16 +1,6 @@
-let andarD
-
-
-function preload {
-   
-        andarD = loadImage('img/run_direito/Run__000.png');
-   
-
-}
 
 function setup() {
   createCanvas(1200, 540);
-   image(andarD,100, 100, 30,30);
 }
 
 
