2615326ab72b02d91e926cdc3155635ed00ad9c0
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-27 09:54:24 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -2,21 +2,21 @@
 let vetor =[];
 var carregando=0;
 let boneco;
-function preload() {
+/*function preload() {
   for(var i=0; i<9; i++){
   vetor[i] = loadImage('img/run_direito/Run__00'+i+'.png');
   }
-}
+}*/
 function setup() {
   createCanvas(1200, 540);
   
 }
 
 function draw() {
-  boneco = vetor[carregando];
+  /*boneco = vetor[carregando];
   
   if(carregando ==8){
     carregando = 0;
   }
-  carregando++;
+  carregando++;*/
 }
