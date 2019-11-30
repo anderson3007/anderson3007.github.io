7dee43114070644f83136735712884d523170598
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-29 22:03:43 -0300

    Update midia.js

--- midia.js
+++ midia.js
@@ -13,11 +13,11 @@ function preload() {
   livro = loadImage('img/livro.png');
   for (var i=0;i<=9;i++){
     andarD [i] = loadImage('img/run_direito/Run__00'+i+'.png');
-    andarE [i] = loadImage('img/run_esquerdo/Runv__00'+i+'png');
-    morrerD[i] = loadImage('img/dead_direito/Dead__00'+i+'png');
-    morrerE[i] = loadImage('img/dead_esquerdo/Deadv__00'+i+'png');
-    pararD [i] = loadImage('img/idle_direito/Idle__00'+i+'png');
-    pararE [i] = loadImage('img/idle_esquerdo/Idlev__00'+i+'png');
+    andarE [i] = loadImage('img/run_esquerdo/Runv__00'+i+'.png');
+    morrerD[i] = loadImage('img/dead_direito/Dead__00'+i+'.png');
+    morrerE[i] = loadImage('img/dead_esquerdo/Deadv__00'+i+'.png');
+    pararD [i] = loadImage('img/idle_direito/Idle__00'+i+'.png');
+    pararE [i] = loadImage('img/idle_esquerdo/Idlev__00'+i+'.png');
   }
 }
 
