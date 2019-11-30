08edbbe514f766f82d8acb4b7b2e39c7fe20eaca
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-29 22:30:57 -0300

    Update midia.js

--- midia.js
+++ midia.js
@@ -18,36 +18,36 @@ function preload() {
     morrerD[i] = loadImage('img/dead_direito/Dead__00'+i+'.png');
     morrerE[i] = loadImage('img/dead_esquerdo/Deadv__00'+i+'.png');
     pararD [i] = loadImage('img/idle_direito/Idle__00'+i+'.png');
-    pararE [i] = loadImage('img/idle_esquerdo/Idlev__00'+i+'.png');
+    
   }
 }
 
 
-function animaUse(animacao) {
+function animaUse() {
   
   if (animacao == 1) {
-      image(andarD[velocidade%9], x, y, 30,30); 
+      image(andarD[velocidade%8], x, y, 30,30); 
   } else {
       
       if (animacao == 2) {
-          image(andarE[velocidade%9], x, y, 30,30);
+          image(andarE[velocidade%8], x, y, 30,30);
       } else {
 
           if (animacao == 3) {
-              image(morrerD[velocidade%9], x, y, 30,30);           
+              image(morrerD[velocidade%8], x, y, 30,30);           
           } else {
 
               if (animacao == 4){
-                  image(morrerE[velocidade%9], x, y, 30,30);           
+                  image(morrerE[velocidade%8], x, y, 30,30);           
               } else {
                   
-                  image(pararD[velocidade%9], x, y, 30,30);
+                  image(pararD[velocidade%8], x, y, 30,30);
                   
                 }  
             }
         }  
     }
-  if (tempo>5) {
+  if (tempo>3) {
     velocidade++;
     tempo=0;
   } else {
