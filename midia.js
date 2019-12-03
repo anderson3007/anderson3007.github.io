21fffb233e596b9e6cf0bd98a34868b4cad98874
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-12-03 18:16:54 -0300

    Update midia.js

--- midia.js
+++ midia.js
@@ -10,51 +10,54 @@ var
 
 function preload() {
   
-  livro = loadImage('img/livro.png');
-  for (var i=0;i<=8;i++){
-    andarD [i] = loadImage('img/run_direito/Run__00'+i+'.png');
-    andarE [i] = loadImage('img/run_esquerdo/Runv__00'+i+'.png');
-    morrerD[i] = loadImage('img/dead_direito/Dead__00'+i+'.png');
-    pararD [i] = loadImage('img/idle_direito/Idle__00'+i+'.png');  
-  }
+      livro = loadImage('img/livro.png');
+      
+      for (var i=0;i<=8;i++){
+            andarD [i] = loadImage('img/run_direito/Run__00'+i+'.png');
+            andarE [i] = loadImage('img/run_esquerdo/Runv__00'+i+'.png');
+            morrerD[i] = loadImage('img/dead_direito/Dead__00'+i+'.png');
+            pararD [i] = loadImage('img/idle_direito/Idle__00'+i+'.png');  
+      }
 }
 
 
 function animaUse() {
  
   
-  if (animacao == 1) {
-      image(andarD[velocidade], x, y, 30,30); 
-  } else {
+      if (animacao == 1) {
+            image(andarD[velocidade], x, y, 30,30); 
+      } 
+          
+      else if (animacao == 2) {
+            image(andarE[velocidade], x, y, 30,30);
+      } 
+
+      else if (animacao == 3) {
+            image(morrerD[velocidade], 500, 200, 90,90);
+            alert("velocidade  "+velocidade+"   tempo:  "+tempo);
+      } 
+
+      else if (animacao == 0){
+            image(pararD[velocidade], x, y, 30,30);    
+      }          
+                
       
-      if (animacao == 2) {
-          image(andarE[velocidade], x, y, 30,30);
-      } else {
+      
+      if (tempo>3) {
+            velocidade++;
+            tempo=0;
+      } 
+      else {
+              tempo++;
+      }
 
-          if (animacao == 3) {
-                fill('#E4E7E7');
-                stroke('white');
-                strokeWeight(1);
-                rect(350, 100, 500, 300, 10); 
-                image(morrerD[velocidade], 500, 200, 90,90);
-                alert("velocidade  "+velocidade+"   tempo:  "+tempo)
 
-          } else {
-              image(pararD[velocidade], x, y, 30,30);    
-            }
-        }  
-    }
-  animacao = 0;
-  if (tempo>3) {
-    velocidade++;
-    tempo=0;
-  } else {
-      tempo++;
-    }
-    if (velocidade >=8) {
-      velocidade=0;
-    }
- 
+      if (velocidade >=8) {
+            velocidade=0;
+      }
+     
+
+      animacao = 0;
 }
 
 
