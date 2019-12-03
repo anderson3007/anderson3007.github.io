5e90f09056a288d9605817eb4891ea2f0659f773
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-12-03 14:23:12 -0300

    Update midia.js

--- midia.js
+++ midia.js
@@ -7,44 +7,46 @@ var
   tempo = 0,
   velocidade=0,
   animacao = 0;
-var m=0,t=0;
 
 function preload() {
+  
   livro = loadImage('img/livro.png');
   for (var i=0;i<=8;i++){
     andarD [i] = loadImage('img/run_direito/Run__00'+i+'.png');
     andarE [i] = loadImage('img/run_esquerdo/Runv__00'+i+'.png');
     morrerD[i] = loadImage('img/dead_direito/Dead__00'+i+'.png');
-    pararD [i] = loadImage('img/idle_direito/Idle__00'+i+'.png');
-    
+    pararD [i] = loadImage('img/idle_direito/Idle__00'+i+'.png');  
   }
 }
 
 
 function animaUse() {
+  animacao = 0;
   
   if (animacao == 1) {
-                
-                image(andarD[velocidade%8], x, y, 30,30); 
+      image(andarD[velocidade%8], x, y, 30,30); 
   } else {
       
       if (animacao == 2) {
-          
-                image(andarE[velocidade%8], x, y, 30,30);
+          image(andarE[velocidade%8], x, y, 30,30);
       } else {
 
           if (animacao == 3) {
-            for(var i=0;i<9; i++){
+            for(var i=0;i<8; i++){
                 fill('#E4E7E7');
                 stroke('white');
                 strokeWeight(1);
                 rect(350, 100, 500, 300, 10); 
-                //image(morrerD[i], 480, 200, 90,90);
-               
-             }
-          }
-              else {
-            
+                image(morrerD[m%9], 500, 200, 90,90);
+                if(t>10){
+                    i--;
+                    m++;
+                }else{
+                  t++
+                }
+            }
+              
+          } else {
               image(pararD[velocidade%9], x, y, 30,30);    
             }
         }  
@@ -56,6 +58,10 @@ function animaUse() {
   } else {
       tempo++;
     }
+}
+
+function fase01() {
+
 }
 
 animacao = 0;
